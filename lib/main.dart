dfc84964abab4c6aa9c2a09b5575dd86d1cf5b2d
Angélica Campos <angelicacampos.dev@gmail.com>
2023-02-13 17:03:48 -0300

    criado crud para serviço

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:app_barbearia/pages/pageHome.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      localizationsDelegates: [
+        GlobalWidgetsLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [Locale("pt", "BR")],
+      debugShowCheckedModeBanner: false,
+      title: 'Teste',
+      home: PageHome(),
+    );
+  }
+}
